78429631760a1088a4672274ad0955363d3147bd
benoitlefebvre99 <benlef99@gmail.com>
2020-01-31 12:02:50 +0100

    add comments

--- webserver/declaration.h
+++ webserver/declaration.h
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+//Déclaration des constantes
 #define DOMAINEIP AF_INET
 #define TYPE SOCK_STREAM
 #define PROTOCOL 0
@@ -11,4 +12,5 @@
 
 struct sockaddr_in saddr;
 
+//déclaration des fonctions externes 'public'
 int creationSocket();
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,8 +1,10 @@
 #include "declaration.h"
 
+//Déclaration des fonctions internes
 int attachementSocket(int socket_serveur);
 int launchListenning(int socket_serveur);
 
+//Fonction qui génère une socket
 int creationSocket(){
     int socket_serveur = socket(DOMAINEIP, TYPE, PROTOCOL);
 
@@ -14,7 +16,9 @@ int creationSocket(){
     return attachementSocket(socket_serveur);
 }
 
+//Fonction qui gère l'attachement de la Socket
 int attachementSocket(int socket_serveur){
+    //Activation des options de réutilisation
     int optVal = 1;
     if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(int)) == -1){
         perror("Erreur lors de l'identification par setsockopt.\n");
@@ -25,13 +29,14 @@ int attachementSocket(int socket_serveur){
     saddr.sin_port = htons(FINALPROTOCOL);
     saddr.sin_addr.s_addr = INADDR_ANY;
 
+    //Attribution de la socket aux interfaces réseaux
     if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
         perror("Erreur lors de l'attachement de la Socket. \n");
         return -3;
     }
     return launchListenning(socket_serveur);
 }
-
+//Lancement des écoutes de connexion
 int launchListenning(int socket_serveur){
     if(listen(socket_serveur, FINALPROTOCOL) == -1){
         perror("Erreur du lancement de l'écoute de connexion. \n");
