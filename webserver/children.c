e0346a6bab04001c9188422e57e57771127a8268
benoitlefebvre99 <benlef99@gmail.com>
2020-02-23 20:15:26 +0100

    ça avance

--- webserver/children.c
+++ webserver/children.c
@@ -3,6 +3,9 @@
 int launchChild(int socket_client){
     int pid = fork();
 
+    const char * GOOD_REQUEST = "GET / HTTP/1.1\r\n";
+    const char * BAD_REQUEST = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bas request\r\n";
+
     if(pid){
         if(close(socket_client) == -1){
             perror("Erreur lors de la création du processus fils");
@@ -20,8 +23,12 @@ int launchChild(int socket_client){
     do {
         memset(reception, 0, sizeof(reception));
         while(fgets(reception, 1024, recept) != NULL){
-            fprintf(recept, "\n<MonServeur>");
-            fputs(reception, recept);
+            //fprintf(recept, "\n<MonServeur>");
+            //fputs(reception, recept);
+            printf("%s", reception);
+            if(!strcmp(GOOD_REQUEST, reception)){
+                fputs(BAD_REQUEST, recept);
+            }
         }
         fflush(recept);
     }while(1);
