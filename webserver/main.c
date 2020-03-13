f35d86ee6a63bae1c646e0ff68012193c8c439d5
BenoitLefebvre99 <benlef99@gmail.com>
2020-03-13 09:56:30 +0100

    add fonctionne

--- webserver/main.c
+++ webserver/main.c
@@ -4,18 +4,16 @@ int socket_serveur;
 
 int main(void){
     socket_serveur = creationSocket();
-    if(socket_serveur > 0){
-        printf("Le serveur a bien été créé au port %d.\n", FINALPROTOCOL);
-    }
 
     int erreur = initialiser_signaux();
 
     while(1){
-        if(erreur != 0 ) {
-            afficher_erreur(erreur);
-            exit(1);
-        }
         erreur = launchListenning(socket_serveur);
+
+        if(erreur != 0) {
+            afficher_erreur(erreur);
+
+        }
     }
 
 }
